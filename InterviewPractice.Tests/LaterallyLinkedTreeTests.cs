b8a89984cd382d57ca17317f63b86623e8bc490e
Martin Kuhne <mkuhne@outlook.com>
2015-01-13 12:54:16 -0800

    LaterallyLinkedTree

---
+++ InterviewPractice.Tests/LaterallyLinkedTreeTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace InterviewPractice.Tests
+{
+    [TestClass]
+    public class LaterallyLinkedTreeTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var n1 = new LaterallyLinkedTree<int>.Node<int>(1);
+            var n2 = new LaterallyLinkedTree<int>.Node<int>(2);
+            var n3 = new LaterallyLinkedTree<int>.Node<int>(3);
+            n1.Edges.Add(n2);
+            n1.Edges.Add(n3);
+
+            LaterallyLinkedTree<int>.Linkify(n1);
+            Assert.AreEqual(n2.Right, n3);
+        }
+    }
+}
--- InterviewPractice/LaterallyLinkedTree.cs
+++ InterviewPractice/LaterallyLinkedTree.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace InterviewPractice
 {
@@ -13,11 +14,46 @@ namespace InterviewPractice
         {
             public T Value { get; set; }
             public List<Node<T>> Edges;
+            
+            // The distinguishing factor of this class is the lateral pointer
+            // (pointer to the next node at the same level)
+            public Node<T> Right;
 
             public Node(T value)
             {
                 this.Value = value;
                 this.Edges = new List<Node<T>>();
+                this.Right = null;
+            }
+        }
+
+        /// <summary>
+        /// This solution is relatively easy to write, but 
+        /// </summary>
+        /// <param name="Root"></param>
+        public static void Linkify(Node<T> Root)
+        {
+            var neighbors = new Dictionary<int, Node<T>>();
+            var toVisit = new Queue<KeyValuePair<int, Node<T>>>();
+            toVisit.Enqueue(new KeyValuePair<int, Node<T>>(1, Root));
+            while (toVisit.Any())
+            {
+                var current = toVisit.Dequeue();
+                var level = current.Key;
+                var node = current.Value;
+
+                if (neighbors.ContainsKey(level))
+                {
+                    var left = neighbors[level];
+                    left.Right = node;
+                    neighbors.Remove(level);
+                }
+                neighbors.Add(level, node);
+
+                foreach (var edge in node.Edges)
+                {
+                    toVisit.Enqueue(new KeyValuePair<int, Node<T>>(level + 1, edge));
+                }
             }
         }
          
