22a8eb34599c315b3ac25534787d26ed2dad7983
Martin Kuhne <mkuhne@outlook.com>
2015-01-12 19:34:21 -0800

    doc pass

--- InterviewPractice/Permutations.cs
+++ InterviewPractice/Permutations.cs
@@ -7,6 +7,14 @@ namespace InterviewPractice
     /// <summary>
     /// Provide all permutations of a string
     /// </summary>
+    /// <remarks>
+    /// I attempted this as a "recursion lite" implementation. I am calling it recursion lite
+    /// because it does not call into itself, but it does need a stack to remember any
+    /// context to return to (which is what calling into itself also does)
+    /// This solution is fairly resource intensive - it misses the original's intent of backtracking.
+    /// Needs improvment, but moving on to other exercises for now
+    /// </remarks>
+    /// <seealso cref="http://www.geeksforgeeks.org/write-a-c-program-to-print-all-permutations-of-a-given-string/"/>
     public class Permutations : IEnumerable<string>
     {
         private class PermContext
@@ -42,6 +50,7 @@ namespace InterviewPractice
                     var swap = ar[context.StartIndex];
                     ar[context.StartIndex] = ar[index];
                     ar[index] = swap;
+
                     if ((context.EndIndex - context.StartIndex) <= 1)
                     {
                         string result = new string(ar);
@@ -52,6 +61,7 @@ namespace InterviewPractice
                         workItems.Push(new PermContext { Value = new string(ar), StartIndex = context.StartIndex + 1, EndIndex = context.EndIndex });
                     }
 
+                    // I don't think we need to swap back because we copied this stuff into a new buffer
                     swap = ar[context.StartIndex];
                     ar[context.StartIndex] = ar[index];
                     ar[index] = swap;
