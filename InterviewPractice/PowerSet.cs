f798838fd16a530fc8d3aed7fcb3596bafd49d58
Martin Kuhne <mkuhne@outlook.com>
2015-02-18 20:56:36 -0800

    PowerSet

---
+++ InterviewPractice/PowerSet.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterviewPractice
+{
+    public class PowerSetHelper
+    {
+        public static List<string> PowerSet(string input)
+        {
+            var result = new List<string>();
+            var ar = input.ToCharArray();
+            var setSize = ar.Length;
+
+            // note overflow risk
+            var resultCount = Convert.ToInt32(Math.Pow(2, setSize));
+
+            result.Add(string.Empty);
+            if (setSize == 0)
+            {
+                return result;
+            }
+
+            result.Add(input);
+            var sb = new StringBuilder();
+            foreach (var iter in Enumerable.Range(1, resultCount - 2))
+            {
+                var index = 0;
+                var bitmask = iter;
+                while (bitmask > 0)
+                {
+                    if ((bitmask & 1) == 1)
+                    {
+                        sb.Append(ar[index]);
+                    }
+                    bitmask >>= 1;
+                    index++;
+                }
+                result.Add(sb.ToString());
+                sb.Clear();
+            }
+            return result;
+        }
+    }
+}
---
+++ InterviewPractice/WordCount.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterviewPractice
+{
+    public class WordCount
+    {
+        /// <summary>
+        /// One Problem per function
+        /// Design for extensibility
+        /// 
+        /// Helper function to determine if a chacater is whitespace
+        /// </summary>
+        /// <param name="input"></param>
+        /// <returns></returns>
+        public static bool isWhiteSpace(this char input)
+        {
+            return (input == ' ');
+        }
+
+        public static uint WordCount(char[] input)
+        {
+            var len = input.Length;
+            uint count = 0;
+            bool scanningWord = false;
+            foreach (char iter in input)
+            {
+                if (isWhiteSpace(iter))
+                {
+                    if (scanningWord == true)
+                    {
+                        count++;
+                        scanningWord = false;
+                    }
+                }
+                else
+                {
+                    if (scanningWord == false)
+                    {
+                        scanningWord = true;
+                    }
+                }
+            }
+
+            // account for encountering the end of the string
+            if (scanningWord)
+            {
+                count++;
+            }
+
+            return count;
+        }
+    }
+}
